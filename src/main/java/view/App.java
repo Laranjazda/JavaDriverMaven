442bf29a8eeea76792ed8d7d7c0205fe119ee1f1
laranjazda <carloseng.jr@gmail.com>
2021-06-04 22:30:23 -0300

    first commit

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -1,9 +1,27 @@
 package view;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import model.Produto;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class App {
     public static void main(String[] args) {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
+        MongoClient mongoClient = new MongoClient("localhost", MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+
+        MongoDatabase database = mongoClient.getDatabase("mongo_connection")
+                .withCodecRegistry(pojoCodecRegistry);
+
+        MongoCollection<Produto> collection = database.getCollection("Produto", Produto.class);
+
+        collection.insertOne(new Produto(3,"Arroz",5));
     }
 }
